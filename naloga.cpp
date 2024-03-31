113d2c5aacca509d7dec5015d91e9f4502f2b230
T5Requiem <ziga.klepej@student.um.si>
2024-03-31 22:26:07 +0200

    Narejena metoda Izpis_Stevil, ki izpiše urejena števila v izhodno datoteko out.txt

--- naloga.cpp
+++ naloga.cpp
@@ -6,6 +6,10 @@ bool Branje_Stevil(vector<int> &vec, const char s[]) {
 }
 
 void Izpis_Stevil(int* polje, unsigned int velikost) {
+    ofstream output("out.txt");
+
+	for (int i = 0; i<velikost; i++)
+		output << polje[i] << ' ';
 }
 
 void countingSort(unsigned char arr[], int size, int exp) {
