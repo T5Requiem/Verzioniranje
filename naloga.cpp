677b9ba94139868c9de6be0843ec7c47011b0ae4
T5Requiem <ziga.klepej@student.um.si>
2024-03-31 22:33:31 +0200

    narejena metoda Branje_Stevil, ki prebere števila iz vhodne datoteke

--- naloga.cpp
+++ naloga.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 bool Branje_Stevil(vector<int> &vec, const char s[]) {
+    ifstream input(s);
+	int st;
+
+	if (!input.is_open()) {
+		return false;
+	}
+
+	while (!input.eof()) {
+		input >> st;
+		vec.push_back(st);
+		while (isspace(input.peek())) input.get();
+	}
+	input.close();
+	return true;
 }
 
 void Izpis_Stevil(int* polje, unsigned int velikost) {
