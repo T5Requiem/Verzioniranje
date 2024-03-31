8804c80d4e1a570bd0c6e82af3ffe286ba0c62f1
T5Requiem <ziga.klepej@student.um.si>
2024-03-31 22:55:21 +0200

    Narejena metoda CountingSort in začetek main

--- naloga.cpp
+++ naloga.cpp
@@ -27,12 +27,33 @@ void Izpis_Stevil(const vector<unsigned char> &vec) {
 }
 
 void countingSort(vector<unsigned char>& A, int eksponent) {
-    
+    vector<unsigned char> output(A.size());
+    vector<int> count(256, 0);
+
+    for (int i = 0; i < A.size(); i++) {
+        count[(A[i] >> eksponent) & 1]++;
+    }
+
+    for (int i = 1; i < 256; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = A.size() - 1; i >= 0; i--) {
+        output[count[(A[i] >> eksponent) & 1] - 1] = A[i];
+        count[(A[i] >> eksponent) & 1]--;
+    }
+
+    for (int i = 0; i < A.size(); i++) {
+        A[i] = output[i];
+    }
 }
 
 void radixSort(unsigned char A[], int velikost) {
 }
 
 int main(int argc, const char* argv[]) {
-    
+    vector<unsigned char> A;
+
+    if (argc < 2) return 0;
+    if (!Branje_Stevil(A, argv[1])) return 0;
 }
